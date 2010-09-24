3f57a6e0e030d5c71faebef24037c9780c471cc8
snusnu <gamsnjaga@gmail.com>
2010-09-24 03:56:08 +0200

    This is awesome :P

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-WARNING: This is currently not working
-======================================
-
-The tasks are currently still a bit broken, we're working hard on it!
-
 Setting up a DataMapper development environment
 -----------------------------------------------
 
@@ -63,24 +58,207 @@ You can choose any name at that prompt, or you can just hit enter to
 accept the default. The provided thor tasks explicitly define their
 namespace to be 'dm'.
 
-The available thor tasks
----------------------------
 
-Once you have the thor tasks available on your machine, you can pull
-down the DataMapper sources, update them and run the specs using an rvm
-setup. The following taks are currently available:
+## dm-dev
 
-    thor dm:sync     # syncs all datamapper repos to DM_ROOT (or Dir.pwd)
-    thor dm:spec     # runs specs on all RUBY_VERSIONS
-    thor dm:release  # release all datamapper gems to rubygems
+The following describes the new DM development tasks and shows how you can use them. These tasks greatly simplify the management of DM related source code and hopefully also help interested contributors to get a fully functional DM development up and running fast. By invoking very few commands, contributors can verify if their patch(es) meet the DM quality guidelines, aka, do specs pass on all supported platforms?
 
-Developing a patch for DataMapper
----------------------------------
+## Totally isolated
 
-So you found a bug in one (or more) of the DataMapper gems and you think
-that you know how to fix it.
+The following tasks don't affect the system gems *at all*. Nor do they mess with any rvm ruby specific (system)gem(set). By default, *everything* will be bundled below `"#{Dir.pwd}/BUNDLE_ROOT"`, you can alter the install location by passing the `BUNDLE_ROOT=/path/to/bundle/root` ENV var. `BUNDLE_ROOT` contains separate folders for every ruby in use.
 
-Please don't let the fact that these instructions currently end
-here, stop you from working on your patch! We'll make sure to update
-this README once the tasks actually work reliably!
+This means that once all dependencies are bundled for any given ruby, there's no need to clean anything between spec runs. Also, no re-bundling needs to happen before spec runs since everything is already bundled. Of course, the bundles can be updated manually, to ensure that the code under test is up to date. In the near future, a command that tells you exactly which repos need updating, will be included.
+
+The tasks make sure that you're always testing against local sources. This is very important if you're developing patches that touch multiple DM repositories. Testing against local sources only, will make sure that the code still works with all your modifications to potentially more than one DM repository.
+
+To achieve this, the bundle tasks first create a `Gemfile.local` for every repository that includes a Gemfile (or isn't otherwise ignored), and then copies that file to `Gemfile.ruby_version.local` where *ruby_version* is any of the specified rubies to use. This is done because bundler automatically creates a `Gemfile.lock` after `bundle install`. In our case that leaves us with files like `Gemfile.1.9.2.local` and `Gemfile.1.9.2.local.lock`. That's necessary, because otherwise bundler confuses the the `BUNDLE_PATH` to use. Every command executed by the bundle tasks explicitly passes `BUNDLE_PATH=/path/to/BUNDLE_ROOT/ruby_version` and `BUNDLE_GEMFILE=/path/to/Gemfile.ruby_version.local` as environment variables, to make sure that the right (local) bundle is used.
+
+## Running specs
+
+The spec task uses the bundled DM sources to run the specs for all specified gems against all specified rubies and adapters. While running, it prints out a matrix, that shows for every ruby and every adapter if the specs `pass` or `fail`.
+
+## Common options
+
+Every task can be configured with a few environment variables. The rubies to use can be altered by passing something like `RUBIES=1.8.7,1.9.2,rbx`. When given, `INCLUDE=dm-core,dm-validations` will make sure that only these two gems are used. When `INCLUDE` is left out, *all* (not ignored) gems will be used. Passing `EXCLUDE=dm-tags,dm-is-tree` will use all but those two gems.
+
+
+## The available ruby API
+
+Currently, environment variables are the only way to configure these API
+calls. It's planned to map those environment variables to a regular ruby
+option hash in the future.
+
+    DM.sync           # :verbose => false
+    DM.bundle_install # :verbose => false
+    DM.bundle_update  # :verbose => false
+    DM.spec           # :verbose => false
+    DM.implode        # :verbose => false
+
+## The available thor tasks
+
+    thor dm:sync
+    thor dm:bundle:install
+    thor dm:bundle:update
+    thor dm:spec
+    thor dm:implode
+
+## IRB session
+
+The following IRB session demonstrate a typical workflow. The API used in this session can also be invoked via system wide thor tasks.
+
+    ree-1.8.7-2010.02@datamapper mungo:dm-dev snusnu$ irb -r dev_tasks.rb
+    ree > DM.sync
+    <GitHub::User name="DataMapper">
+    [01/38] Pulling dm-core
+    [02/38] Pulling do
+    [03/38] Pulling extlib
+    [04/38] Pulling datamapper.github.com
+    [05/38] Pulling data_mapper
+    [06/38] Pulling dm-rails
+    [07/38] Pulling dm-active_model
+    [08/38] Pulling dm-transactions
+    [09/38] Pulling dm-mysql-adapter
+    [10/38] Pulling dm-postgres-adapter
+    [11/38] Pulling dm-sqlite-adapter
+    [12/38] Pulling dm-do-adapter
+    [13/38] Pulling dm-yaml-adapter
+    [14/38] Pulling dm-adjust
+    [15/38] Pulling dm-aggregates
+    [16/38] Pulling dm-ar-finders
+    [17/38] Pulling dm-cli
+    [18/38] Pulling dm-constraints
+    [19/38] Pulling dm-is-list
+    [20/38] Pulling dm-is-nested_set
+    [21/38] Pulling dm-is-remixable
+    [22/38] Pulling dm-is-searchable
+    [23/38] Pulling dm-is-state_machine
+    [24/38] Pulling dm-is-tree
+    [25/38] Pulling dm-is-versioned
+    [26/38] Pulling dm-migrations
+    [27/38] Pulling dm-observer
+    [28/38] Pulling dm-serializer
+    [29/38] Pulling dm-sweatshop
+    [30/38] Pulling dm-tags
+    [31/38] Pulling dm-timestamps
+    [32/38] Pulling dm-types
+    [33/38] Pulling dm-validations
+    [34/38] Pulling dm-models
+    [35/38] Pulling dm-ferret-adapter
+    [36/38] Pulling dm-rest-adapter
+    [37/38] Pulling rails_datamapper
+    [38/38] Pulling dm-dev
+     => nil
+    ree > DM.bundle_install
+    <GitHub::User name="DataMapper">
+    [01/38] [1.8.7] bundle install dm-core
+    [01/38] [1.9.2] bundle install dm-core
+    [02/38] [1.8.7] bundle install do SKIPPED - because it's missing a Gemfile
+    [02/38] [1.9.2] bundle install do SKIPPED - because it's missing a Gemfile
+    [03/38] [1.8.7] bundle install extlib SKIPPED - because it's missing a Gemfile
+    [03/38] [1.9.2] bundle install extlib SKIPPED - because it's missing a Gemfile
+    [04/38] [1.8.7] bundle install datamapper.github.com SKIPPED - because it's ignored
+    [04/38] [1.9.2] bundle install datamapper.github.com SKIPPED - because it's ignored
+    [05/38] [1.8.7] bundle install data_mapper SKIPPED - because it's ignored
+    [05/38] [1.9.2] bundle install data_mapper SKIPPED - because it's ignored
+    [06/38] [1.8.7] bundle install dm-rails
+    [06/38] [1.9.2] bundle install dm-rails
+    [07/38] [1.8.7] bundle install dm-active_model
+    [07/38] [1.9.2] bundle install dm-active_model
+    [08/38] [1.8.7] bundle install dm-transactions
+    [08/38] [1.9.2] bundle install dm-transactions
+    [09/38] [1.8.7] bundle install dm-mysql-adapter
+    [09/38] [1.9.2] bundle install dm-mysql-adapter
+    [10/38] [1.8.7] bundle install dm-postgres-adapter
+    [10/38] [1.9.2] bundle install dm-postgres-adapter
+    [11/38] [1.8.7] bundle install dm-sqlite-adapter
+    [11/38] [1.9.2] bundle install dm-sqlite-adapter
+    [12/38] [1.8.7] bundle install dm-do-adapter
+    [12/38] [1.9.2] bundle install dm-do-adapter
+    [13/38] [1.8.7] bundle install dm-yaml-adapter
+    [13/38] [1.9.2] bundle install dm-yaml-adapter
+    [14/38] [1.8.7] bundle install dm-adjust
+    [14/38] [1.9.2] bundle install dm-adjust
+    [15/38] [1.8.7] bundle install dm-aggregates
+    [15/38] [1.9.2] bundle install dm-aggregates
+    [16/38] [1.8.7] bundle install dm-ar-finders
+    [16/38] [1.9.2] bundle install dm-ar-finders
+    [17/38] [1.8.7] bundle install dm-cli
+    [17/38] [1.9.2] bundle install dm-cli
+    [18/38] [1.8.7] bundle install dm-constraints
+    [18/38] [1.9.2] bundle install dm-constraints
+    [19/38] [1.8.7] bundle install dm-is-list
+    [19/38] [1.9.2] bundle install dm-is-list
+    [20/38] [1.8.7] bundle install dm-is-nested_set
+    [20/38] [1.9.2] bundle install dm-is-nested_set
+    [21/38] [1.8.7] bundle install dm-is-remixable
+    [21/38] [1.9.2] bundle install dm-is-remixable
+    [22/38] [1.8.7] bundle install dm-is-searchable
+    [22/38] [1.9.2] bundle install dm-is-searchable
+    [23/38] [1.8.7] bundle install dm-is-state_machine
+    [23/38] [1.9.2] bundle install dm-is-state_machine
+    [24/38] [1.8.7] bundle install dm-is-tree
+    [24/38] [1.9.2] bundle install dm-is-tree
+    [25/38] [1.8.7] bundle install dm-is-versioned
+    [25/38] [1.9.2] bundle install dm-is-versioned
+    [26/38] [1.8.7] bundle install dm-migrations
+    [26/38] [1.9.2] bundle install dm-migrations
+    [27/38] [1.8.7] bundle install dm-observer
+    [27/38] [1.9.2] bundle install dm-observer
+    [28/38] [1.8.7] bundle install dm-serializer
+    [28/38] [1.9.2] bundle install dm-serializer
+    [29/38] [1.8.7] bundle install dm-sweatshop
+    [29/38] [1.9.2] bundle install dm-sweatshop
+    [30/38] [1.8.7] bundle install dm-tags
+    [30/38] [1.9.2] bundle install dm-tags
+    [31/38] [1.8.7] bundle install dm-timestamps
+    [31/38] [1.9.2] bundle install dm-timestamps
+    [32/38] [1.8.7] bundle install dm-types
+    [32/38] [1.9.2] bundle install dm-types
+    [33/38] [1.8.7] bundle install dm-validations
+    [33/38] [1.9.2] bundle install dm-validations
+    [34/38] [1.8.7] bundle install dm-models SKIPPED - because it's missing a Gemfile
+    [34/38] [1.9.2] bundle install dm-models SKIPPED - because it's missing a Gemfile
+    [35/38] [1.8.7] bundle install dm-ferret-adapter SKIPPED - because it's ignored
+    [35/38] [1.9.2] bundle install dm-ferret-adapter SKIPPED - because it's ignored
+    [36/38] [1.8.7] bundle install dm-rest-adapter
+    [36/38] [1.9.2] bundle install dm-rest-adapter
+    [37/38] [1.8.7] bundle install rails_datamapper SKIPPED - because it's ignored
+    [37/38] [1.9.2] bundle install rails_datamapper SKIPPED - because it's ignored
+    [38/38] [1.8.7] bundle install dm-dev SKIPPED - because it's ignored
+    [38/38] [1.9.2] bundle install dm-dev SKIPPED - because it's ignored
+     => nil
+    ree > exit
+    ree-1.8.7-2010.02@datamapper mungo:dm-dev snusnu$ RUBIES=1.8.7,1.9.2 INCLUDE=dm-validations ADAPTERS=sqlite,postgres,mysql irb -r dev_tasks.rb
+    ree > DM.sync
+    <GitHub::User name="DataMapper">
+    [1/1] Pulling dm-validations
+     => nil
+    ree > DM.implode
+    <GitHub::User name="DataMapper">
+    [1/1] Deleting dm-validations
+     => nil
+    ree > DM.sync
+    <GitHub::User name="DataMapper">
+    [1/1] Cloning dm-validations
+     => nil
+    ree > DM.bundle_install
+    <GitHub::User name="DataMapper">
+    [1/1] [1.8.7] bundle install dm-validations
+    [1/1] [1.9.2] bundle install dm-validations
+     => nil
+    ree > DM.bundle_update
+    <GitHub::User name="DataMapper">
+    [1/1] [1.8.7] bundle update dm-validations
+    [1/1] [1.9.2] bundle update dm-validations
+     => nil
+    ree > DM.spec
+    <GitHub::User name="DataMapper">
+
+    h2. dm-validations
+
+    | RUBY  | sqlite | postgres | mysql |
+    | 1.8.7 | pass | pass | pass |
+    | 1.9.2 | pass | pass | pass |
+     => nil
+    ree >
 
---
+++ tasks.rb
@@ -0,0 +1,709 @@
+#!/usr/bin/env ruby -Ku
+# encoding: utf-8
+
+require 'set'
+require 'fileutils'
+require 'pathname'
+
+require 'thor'
+require 'addressable/uri'
+require 'ruby-github'
+
+class Project
+
+  def self.command_names
+    %w[ sync bundle:install bundle:update spec release implode]
+  end
+
+  def self.command_name(name)
+    command_fragments(name).join('_')
+  end
+
+  def self.command_class_name(name)
+    command_fragments(name).map { |fragment| fragment.capitalize }.join('::')
+  end
+
+  def self.command_fragments(name)
+    name.split(':').map { |fragment| fragment }
+  end
+
+  command_names.each do |name|
+    class_eval <<-RUBY, __FILE__, __LINE__ + 1
+      def self.#{command_name(name)}(options = {})
+        new(options).send(:#{command_name(name)})
+      end
+
+      def #{command_name(name)}
+        self.class.invoke :before, '#{command_name(name)}', env, repos
+        @repos.each_with_index do |repo, index|
+          @logger.next_command
+          command_class('#{name}').new(repo, env, @logger, options[:verbose]).run
+        end
+        self.class.invoke :after, '#{command_name(name)}', env, repos
+      end
+    RUBY
+  end
+
+  attr_reader   :env
+  attr_reader   :root
+  attr_reader   :repos
+  attr_reader   :options
+
+  attr_accessor :commands
+
+  def initialize(options = {})
+    @options  = options
+    @env      = environment_class.new(name)
+    @root     = @env.root
+    @repos    = Repositories.new(@root, name, @env.included, @env.excluded + excluded_repos)
+    @logger   = Logger.new(@repos.count, @options[:verbose])
+    @commands = {}
+  end
+
+  def environment_class
+    Environment
+  end
+
+  def command_class(name)
+    return commands[name] if commands[name]
+    Utils.full_const_get(self.class.command_class_name(name), Command)
+  end
+
+  def self.before(command_name, &block)
+    ((@before ||= {})[command_name] ||= []) << block
+  end
+
+  def self.after(command_name, &block)
+    ((@after ||= {})[command_name] ||= []) << block
+  end
+
+  def self.invoke(kind, name, *args)
+    hooks = instance_variable_get("@#{kind}")
+    return unless hooks && hooks[name]
+    hooks[name].each { |hook| hook.call(*args) }
+  end
+
+  module Utils
+
+    def self.full_const_get(name, root = Object)
+      obj = root
+      namespaces(name).each do |x|
+        # This is required because const_get tries to look for constants in the
+        # ancestor chain, but we only want constants that are HERE
+        obj = obj.const_defined?(x) ? obj.const_get(x) : obj.const_missing(x)
+      end
+      obj
+    end
+
+    def self.namespaced?(const_name)
+      namespaces(const_name).size > 1
+    end
+
+    def self.namespaces(const_name)
+      path = const_name.to_s.split('::')
+      path.shift if path.first.empty?
+      path
+    end
+
+  end
+
+  class Repositories
+
+    include Enumerable
+
+    def initialize(root, user, repos, excluded_repos)
+      @root, @user    = root, user
+      @excluded_repos = excluded_repos
+      @repositories   = fetch(repos).map do |repo|
+        Repository.new(@root, repo)
+      end
+    end
+
+    def each
+      @repositories.each { |repo| yield(repo) }
+    end
+
+  private
+
+    def fetch(repos)
+      GitHub::API.user(@user).repositories.select do |repo|
+        include?(repo, repos)
+      end
+    end
+
+    def include?(repo, repos)
+      if repos
+        repos.include?(repo.name)
+      else
+        !@excluded_repos.include?(repo.name)
+      end
+    end
+
+  end
+
+  class Repository
+
+    attr_reader :path
+    attr_reader :name
+    attr_reader :uri
+
+    def initialize(root, repo)
+      @name = repo.name
+      @path = root.join(@name)
+      @uri  = Addressable::URI.parse(repo.url)
+    end
+
+    def installable?
+      path.join('Gemfile').file?
+    end
+
+  end
+
+  class Environment
+
+    attr_reader :name
+    attr_reader :root
+    attr_reader :included
+    attr_reader :excluded
+    attr_reader :rubies
+    attr_reader :bundle_root
+
+    def initialize(name)
+      @name        = name
+      @root        = Pathname(ENV['ROOT'       ] || Dir.pwd)
+      @bundle_root = Pathname(ENV['BUNDLE_ROOT'] || @root.join(default_bundle_root))
+      @included    = ENV['INCLUDE'] ? normalize(ENV['INCLUDE']) : default_included
+      @excluded    = ENV['EXCLUDE'] ? normalize(ENV['EXCLUDE']) : default_excluded
+      @rubies      = ENV['RUBIES' ] ? normalize(ENV['RUBIES' ]) : default_rubies
+    end
+
+    def default_bundle_root
+      'BUNDLE_ROOT'
+    end
+
+    def default_included
+      nil # means all
+    end
+
+    def default_excluded
+      [] # overwrite in subclasses
+    end
+
+    def default_rubies
+      %w[ 1.8.7 1.9.2 ]
+    end
+
+  private
+
+    def normalize(string)
+      string.gsub(',', ' ').split(' ')
+    end
+
+  end
+
+  class Logger
+
+    def initialize(repo_count, verbose)
+      @total   = repo_count
+      @padding = @total.to_s.length
+      @index   = 0
+      @verbose = verbose
+    end
+
+    def log(repo, action, command = nil, msg = nil)
+      puts '[%0*d/%d] %s %s %s%s' % format(repo, action, command, msg)
+    end
+
+    def next_command
+      @index += 1
+    end
+
+    def format(repo, action, command, msg)
+      [ @padding, @index, @total, action, repo.name, msg, @verbose ? ": #{command}" : '' ]
+    end
+
+  end
+
+  class Command
+
+    attr_reader :repo
+    attr_reader :env
+    attr_reader :root
+    attr_reader :path
+    attr_reader :uri
+    attr_reader :logger
+
+    def initialize(repo, env, logger, verbose = false)
+      @repo    = repo
+      @env     = env
+      @root    = @env.root
+      @path    = @root.join(@repo.name)
+      @uri     = @repo.uri
+      @logger  = logger
+      @verbose = verbose
+    end
+
+    def before
+      # overwrite in subclasses
+    end
+
+    def after
+      # overwrite in subclasses
+    end
+
+    def ignored?
+      ignored_repos.include?(repo.name)
+    end
+
+    def ignored_repos
+      [] # overwrite in subclasses
+    end
+
+    def working_dir
+      path
+    end
+
+    def verbose?
+      @verbose
+    end
+
+    def verbosity
+      verbose? ? verbose : silent
+    end
+
+    def verbose
+    end
+
+    def silent
+      '>& /dev/null'
+    end
+
+    def log(command = nil, msg = nil)
+      logger.log(repo, action, command, msg)
+    end
+
+    class Sync < Command
+
+      def self.new(repo, env, logger, verbose = false)
+        return super unless self == Sync
+        if env.root.join(repo.name).directory?
+          Pull.new(repo, env, logger, verbose)
+        else
+          Clone.new(repo, env, logger, verbose)
+        end
+      end
+
+      attr_reader :git_uri
+
+      def initialize(repo, env, logger, verbose = false)
+        super
+        @git_uri = uri.dup
+        @git_uri.scheme = scheme
+      end
+
+      def run
+        FileUtils.cd(working_dir) do
+          before
+          log(command); system(command)
+          after
+        end
+      end
+
+      def scheme
+        'git'
+      end
+
+      class Clone < Sync
+
+        def command
+          "git clone #{git_uri}.git #{verbosity}"
+        end
+
+        def working_dir
+          root
+        end
+
+        def action
+          'Cloning'
+        end
+
+      end
+
+      class Pull < Sync
+
+        def command
+          "git checkout master #{verbosity}; git pull --rebase #{verbosity}"
+        end
+
+        def action
+          'Pulling'
+        end
+
+      end
+    end
+
+    class Rvm < Command
+
+      attr_reader :rubies
+
+      def initialize(repo, env, logger, verbose = false)
+        super
+        @rubies = env.rubies
+      end
+
+      def run
+        FileUtils.cd(working_dir) do
+          rubies.each do |ruby|
+            before
+            yield(ruby)
+            after
+          end
+        end
+      end
+
+      def command(ruby)
+        "rvm #{ruby} exec bash -c"
+      end
+
+      def action(ruby = nil)
+        "[#{ruby}]"
+      end
+
+    end
+
+    class Bundle < Rvm
+
+      class Install < Bundle
+
+        def bundle_command
+          'install'
+        end
+
+        def action(ruby = nil)
+          "#{super} bundle install"
+        end
+
+      end
+
+      class Update < Bundle
+
+        def bundle_command
+          'update'
+        end
+
+        def action(ruby = nil)
+          "#{super} bundle update"
+        end
+
+      end
+
+      def initialize(repo, env, logger, verbose = false)
+        super
+        @bundle_root = env.bundle_root
+        rubies.each { |ruby| bundle_path(ruby).mkpath }
+      end
+
+      def run
+        super do |ruby|
+          if block_given?
+            yield ruby
+          else
+            if executable?
+              sleep timeout
+              log ruby, command(ruby)
+              make_gemfile(ruby)
+              system command(ruby)
+            else
+              log ruby, command(ruby), "SKIPPED - #{explanation}"
+            end
+          end
+        end
+      end
+
+      def executable?
+        !ignored? && repo.installable?
+      end
+
+      def command(ruby)
+        "#{super} \"#{environment(ruby)} bundle #{bundle_command} #{options} #{verbosity}\""
+      end
+
+      def environment(ruby)
+        "BUNDLE_PATH='#{bundle_path(ruby)}' BUNDLE_GEMFILE='#{gemfile(ruby)}'"
+      end
+
+      def bundle_path(ruby)
+        @bundle_root.join(ruby)
+      end
+
+      def gemfile(ruby)
+        "Gemfile.#{ruby}"
+      end
+
+      def make_gemfile(ruby)
+        gemfile = working_dir.join(gemfile(ruby))
+        unless gemfile.file?
+          FileUtils.cp(working_dir.join('Gemfile.local'), working_dir.join(gemfile(ruby)))
+        end
+      end
+
+      def options
+        nil
+      end
+
+      def timeout
+        0
+      end
+
+      def explanation
+        if ignored?
+          "because it's ignored"
+        elsif !repo.installable?
+          "because it's missing a Gemfile"
+        else
+          "reason unknown"
+        end
+      end
+
+      def log(ruby = nil, command = nil, msg = nil)
+        logger.log(repo, action(ruby), command, msg)
+      end
+
+    end
+
+    class Spec < Bundle
+
+      def initialize(repo, env, logger, verbose = false)
+        super
+      end
+
+      def run
+
+        puts "\nh2. %s\n\n" % repo.name
+        puts '| RUBY  | %s |' % env.adapters.join(' | ')
+
+        super do |ruby|
+
+          print '| %s |' % ruby
+
+          if block_given?
+            yield ruby
+          else
+            log    command(ruby)
+            system command(ruby)
+
+            print ' %s |' % [ $?.success? ? 'pass' : 'fail' ]
+          end
+
+          puts
+
+        end
+
+      end
+
+      def environment(ruby)
+        "#{super} TZ='utc'"
+      end
+
+      def bundle_command
+        'exec rake spec'
+      end
+
+      def action(ruby = nil)
+        "#{super} Testing"
+      end
+
+    end
+
+    class Release < Command
+
+      def run
+        # TODO move to its own command
+        clean_repository(project_name)
+
+        FileUtils.cd(working_dir) do
+          log(command)
+          system(command)
+        end
+      end
+
+      def command
+        'rake release'
+      end
+
+      def action
+        'Releasing'
+      end
+
+    end
+
+    class Implode < Command
+
+      def run
+        log    command
+        system command
+      end
+
+      def command
+        "rm -rf #{repo.name} #{verbosity}"
+      end
+
+      def action
+        'Deleting'
+      end
+
+    end
+
+  end
+
+end
+
+module DataMapper
+
+  class Project < ::Project
+
+    def initialize(options = {})
+      super
+      commands['bundle:install'] = DataMapper::Project::Bundle::Install
+      commands['spec']           = DataMapper::Project::Spec
+    end
+
+    def name
+      'datamapper'
+    end
+
+    def environment_class
+      DataMapper::Project::Environment
+    end
+
+    def excluded_repos
+      %w[ dm-more ]
+    end
+
+    before 'implode' do |env, repos|
+      FileUtils.rm_rf env.bundle_root if env.included.nil?
+    end
+
+    class Environment < ::Project::Environment
+
+      attr_reader :adapters
+
+      def initialize(name)
+        super
+        @adapters ||= ENV['ADAPTERS'] ? normalize(ENV['ADAPTERS']) : default_adapters
+      end
+
+      def default_adapters
+        %w[ in_memory yaml sqlite postgres mysql ]
+      end
+
+      def default_excluded
+        %w[ dm-oracle-adapter dm-sqlserver-adapter ]
+      end
+
+    end
+
+    module Bundle
+
+      def support_lib(ruby)
+        ruby == '1.8.6' ? 'EXTLIB="true"' : ''
+      end
+
+      def adapters(ruby)
+        env.adapters.join(',')
+      end
+
+      def gemfile(ruby)
+        "#{super}#{local_install? ? '.local' : ''}"
+      end
+
+      def ignored_repos
+        %w[ dm-dev data_mapper datamapper.github.com dm-ferret-adapter rails_datamapper ]
+      end
+
+      def timeout
+        2
+      end
+
+      class Install < ::Project::Command::Bundle::Install
+
+        include DataMapper::Project::Bundle
+
+        def environment(ruby)
+          "#{super} #{support_lib(ruby)} ADAPTERS='#{adapters(ruby)}'"
+        end
+
+        def before
+          @local_install = system "rake local_gemfile #{verbosity}"
+        end
+
+        def local_install?
+          @local_install
+        end
+
+      end
+
+    end
+
+    class Spec < ::Project::Command::Spec
+
+      include DataMapper::Project::Bundle
+
+      def run
+        super do |ruby|
+          env.adapters.each do |adapter|
+
+            @adapter = adapter # HACK?
+
+            log    command(ruby) if verbose?
+            system command(ruby)
+
+            print ' %s |' % [ $?.success? ? 'pass' : 'fail' ]
+          end
+        end
+      end
+
+      def environment(ruby)
+        "#{super} #{support_lib(ruby)} ADAPTER='#{@adapter}'"
+      end
+
+      def local_install?
+        working_dir.join('Gemfile.local').file?
+      end
+
+    end
+
+    # The tasks
+    class Tasks < ::Thor
+
+      namespace :dm
+
+      class_option :verbose => :boolean, :default => false
+
+      desc 'sync', 'Sync with the DM repositories'
+      def sync
+        DataMapper::Project.sync(options)
+      end
+
+      desc 'bundle', 'Bundle the DM repositories'
+      def bundle
+        DataMapper::Project.bundle(options)
+      end
+
+      desc 'spec', 'Run specs for DM gems'
+      def spec
+        DataMapper::Project.spec(options)
+      end
+
+      desc 'release', 'Release all DM gems to rubygems'
+      def release
+        DataMapper::Project.release(options)
+      end
+
+    end
+
+  end
+end
+
+DM = DataMapper::Project
+
