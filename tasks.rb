4d89af8ee5f1ddac0a4defefde6575de1009f557
snusnu <gamsnjaga@gmail.com>
2010-09-24 16:22:41 +0200

    Use the ROOT env var in generated system calls too

--- tasks.rb
+++ tasks.rb
@@ -429,7 +429,7 @@ class ::Project
       end
 
       def environment(ruby)
-        "BUNDLE_PATH='#{bundle_path(ruby)}' BUNDLE_GEMFILE='#{gemfile(ruby)}'"
+        "ROOT=#{env.root} BUNDLE_PATH='#{bundle_path(ruby)}' BUNDLE_GEMFILE='#{gemfile(ruby)}'"
       end
 
       def bundle_path(ruby)
