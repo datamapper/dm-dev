211212f4b72bef8a2776c55096b579ac57c07470
snusnu <gamsnjaga@gmail.com>
2010-10-05 18:53:27 +0200

    Cache repo metadata right after fetching from github
    Related to 345500bfa3a1572ba0a01eeca9b91bcef3afdfdc
    
    I couldn't do it earlier because I was offline when
    coding that first part. Unsurprisingly, this was the
    initial motivation for providing offline support in
    the first place :)

--- tasks.rb
+++ tasks.rb
@@ -112,7 +112,7 @@ class ::Project
       if filename.file?
         load_from_yaml(filename)
       else
-        GitHub::API.user(name).repositories
+        load_from_github(filename)
       end
     end
 
@@ -120,10 +120,25 @@ class ::Project
       'dm-dev.yml'
     end
 
+    def load_from_github(filename)
+      cache(GitHub::API.user(name).repositories, filename)
+    end
+
     def load_from_yaml(filename)
       YAML.load(File.open(filename))['repositories'].map do |repo|
         Struct.new(:name, :url).new(repo['name'], repo['url'])
       end
+    end
+
+  private
+
+    def cache(repos, filename)
+      File.open(filename, 'w') do |f|
+        f.write(YAML.dump({
+          'repositories' => repos.map { |repo| { 'name' => repo.name, 'url' => repo.url } }
+        }))
+      end
+      repos
     end
 
   end
